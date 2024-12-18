f7fcd9d775fe0fac2502cd335bbca8d660f2b2b0
Karan Gondaliya <139796588+karangondaliya@users.noreply.github.com>
2024-12-18 18:24:45 +0530

    Initial Commit

---
+++ Employee-Monitoring-System-API/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Employee_Monitoring_System_API.Models;
+
+namespace Employee_Monitoring_System_API.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
